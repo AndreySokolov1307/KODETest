c686d7a733cfc17b196e7367a4a541dda39b1701
Andrei Sokolov <andreysokolov1307@mail.ru>
2024-03-09 14:04:05 +0200

    Initial setup

---
+++ KODETest/KODETest/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KODETest
+//
+//  Created by Андрей Соколов on 09.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
